fe221a9a554184981a36f4e8ee798a8c6def032b
devxinstructor <devx@gmail.com>
2020-01-27 18:55:52 -0600

    First Push

--- src/test/java/tests/ChromeTest2.java
+++ src/test/java/tests/ChromeTest2.java
@@ -8,16 +8,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class ChromeTest2 {
 
     @Test
     public void testAmazon() throws InterruptedException{
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+
         driver.get("https://www.amazon.com/");
-        Thread.sleep(2000);
+
+        //Thread.sleep(2000);
         driver.findElement(By.id("nav-hamburger-menu")).click();
-        Thread.sleep(2000);
+       //Thread.sleep(2000);
 
         WebElement text = driver.findElement(By.id("hmenu-customer-name"));
         Assert.assertTrue(text.isDisplayed());
